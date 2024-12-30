91f081ac88296e37e769cd3ca4632145ffc52324
Harsh <patelharsh749005@gmail.com>
2024-12-30 11:38:32 +0530

    Dashboard deleted and now content is in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import Dashboard from './pages/Dashboard'
+import { useState } from "react";
+import Dashboard from "./pages/Dashboard";
+import Pagination from "./components/Pagination";
+import Navbar from "./components/Navbar";
+import Orders from "./pages/Orders";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Dashboard/>
-    
+      <Navbar />
+      <div className="w-full  flex items-start">
+        <Pagination />
+        <Orders />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
